1c53c9817f64835aff26b3149910a69411146f64
Liamcr <cariou.liam@orange.fr>
2023-08-24 00:31:54 +0200

    add categorie dans controller

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Service\ArticleService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class CategoryController extends AbstractController
 {
     #[Route('/category/{slug}', name: 'category_show')]
-    public function show(): Response
+    public function show(?Category $category): Response
     {
+        if (!$category) {
+            return $this->redirectToRoute('app_home');
+        }
+
         return $this->render('category/show.html.twig', [
-            'controller_name' => 'CategoryController',
+            'category' => $category,
         ]);
     }
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Controller;
-
+use App\Repository\CategoryRepository;
 use App\Repository\ArticleRepository; 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,11 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(ArticleRepository $articleRepo): Response
+    public function index(ArticleRepository $articleRepo, CategoryRepository $categoryRepo): Response
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
             'articles' => $articleRepo->findAll(),
+            'categories' => $categoryRepo->findAll()
         ]);
     }
 }
